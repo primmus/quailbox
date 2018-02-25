9337d4d68fe70dcdec3d351b5c4fd6e2af305053
Daan Spitz <daan@cuckoo.sh>
2018-02-25 23:08:03 +0100

    add qemu opts whitelist

--- quailbox/machinery/qemu.py
+++ quailbox/machinery/qemu.py
@@ -2,9 +2,22 @@
 
 
 class Qemu(object):
+    whitelist = ["arch", "kernel", "machine", "memory"]
+
     def __init__(self, profile=None):
         self.profile = profile
         self.console = None
 
+    def _verify_config(self):
+        config = self.profile.config
+        return all(c in self.whitelist for c in config.keys())
+
     def run(self):
+        if not self._verify_config():
+            raise QemuException
         return self.console
+
+
+class QemuException(Exception):
+    pass
+
